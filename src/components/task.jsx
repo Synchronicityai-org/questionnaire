4c116aea369b5adc3a243d2015372ef3a40fa9f0
vamsisollem <50356552+vamsisollem@users.noreply.github.com>
2024-11-26 13:40:02 -0500

    removed css

--- src/components/task.jsx
+++ src/components/task.jsx
@@ -1,4 +1,3 @@
-import '@components/task.css';
 import {useStore} from '@components/store';
 
 function Task() {
@@ -13,24 +12,25 @@ function Task() {
   return (
     <div className='flex items-center flex-col'>
       {filteredTasks.map((task) => (
-        <div key={task.id} className='task-list'>
+        <div key={task.id} className='bg-very-dark-desaturated-blue text-very-light-gray mb-1 w-[500px] flex items-center p-2.5 rounded-md'>
           <input 
             type="checkbox" 
-            className={task.completed ? 'check-mark-checked': 'check-mark'} 
+            className={`appearance-none w-5 h-5 rounded-full border border-very-light-grayish-blue mr-2 cursor-pointer ${task.completed ? `bg-check-combined bg-no-repeat bg-center` : ''}`}
+
             checked={task.completed}
             onChange={() => handleData(task)}
           />
-          <li className={task.completed ? 'strike': ''}>{task.task}</li>
+          <li className={`list-none ${task.completed ? 'line-through': ''}`}>{task.task}</li>
         </div>
       ))}
-      <div className='footer'>
+      <div className='bg-very-dark-desaturated-blue w-[500px] p-2 flex items-center justify-between rounded-md text-very-light-gray'>
         <span>{filteredTasks.length} Items left</span>
-        <div className='filters'>
-          <button onClick={() => handleTasks('All')}>All</button>
-          <button onClick={() => handleTasks('Active')}>Active</button>
-          <button onClick={() => handleTasks('Completed')}>Completed</button>
+        <div className='space-x-2'>
+          <button onClick={() => handleTasks('All')} className='hover:bg-dark-grayish-blue rounded-md p-2'>All</button>
+          <button onClick={() => handleTasks('Active')} className='hover:bg-dark-grayish-blue rounded-md p-2'>Active</button>
+          <button onClick={() => handleTasks('Completed')} className='hover:bg-dark-grayish-blue rounded-md p-2'>Completed</button>
         </div>
-        <button onClick={clearCompleted}>Clear Completed</button>
+        <button onClick={clearCompleted} className='hover:bg-dark-grayish-blue rounded-md p-2'>Clear Completed</button>
       </div>
     </div>
   );
--- src/server.js
+++ src/server.js
@@ -2,8 +2,15 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 // Firebase config
-
-
+const firebaseConfig = {
+    apiKey: "AIzaSyA_-gxHUt8qaW8cadq38FuqcgK3Va9Jo3E",
+    authDomain: "t-o-d-o-217c3.firebaseapp.com",
+    projectId: "t-o-d-o-217c3",
+    storageBucket: "t-o-d-o-217c3.firebasestorage.app",
+    messagingSenderId: "916024712670",
+    appId: "1:916024712670:web:ac7658c20c9a30f7ff914f",
+    measurementId: "G-JC5NX8PCE2"
+  };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
