122aae2b8e46c0697fd044d78cf31dc0abaaa3cb
vksynchronicityai <info@synchronicityai.org>
2025-03-15 23:14:38 -0400

    ok

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -4,36 +4,5 @@ import { defineAuth } from '@aws-amplify/backend';
  * Define and configure your auth resource
  * @see https://docs.amplify.aws/gen2/build-a-backend/auth
  */
-export const auth = defineAuth({
-  loginWith: {
-    email: true
-  },
-  userAttributes: {
-    // Custom attributes for user role and profile type
-    'custom:profileType': {
-      dataType: 'String',
-      mutable: true
-    },
-    'custom:role': {
-      dataType: 'String',
-      mutable: true
-    },
-    // Standard required attributes
-    email: {
-      mutable: true
-    },
-    givenName: {
-      mutable: true
-    },
-    familyName: {
-      mutable: true
-    },
-    phoneNumber: {
-      mutable: true
-    }
-  },
-  // Enable MFA if needed
-  multifactor: {
-    mode: 'OFF' // Change to 'OPTIONAL' or 'REQUIRED' if you want MFA
-  }
-});
+// Temporarily removing auth configuration to allow recreation
+export const auth = undefined;
