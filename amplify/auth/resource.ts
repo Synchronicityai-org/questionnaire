b028994ed03898aec24c4e452bc50a768787b5ae
vksynchronicityai <info@synchronicityai.org>
2025-03-15 23:20:11 -0400

    changes

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -4,5 +4,37 @@ import { defineAuth } from '@aws-amplify/backend';
  * Define and configure your auth resource
  * @see https://docs.amplify.aws/gen2/build-a-backend/auth
  */
-// Temporarily removing auth configuration to allow recreation
-export const auth = undefined;
+export const auth = defineAuth({
+  loginWith: {
+    email: true
+  },
+  userAttributes: {
+    'custom:profileType': {
+      dataType: 'String',
+      mutable: true
+    },
+    'custom:role': {
+      dataType: 'String',
+      mutable: true
+    },
+    email: {
+      required: true,
+      mutable: true
+    },
+    givenName: {
+      required: true,
+      mutable: true
+    },
+    familyName: {
+      required: true,
+      mutable: true
+    },
+    phoneNumber: {
+      required: false,
+      mutable: true
+    }
+  },
+  multifactor: {
+    mode: 'OFF'
+  }
+});
